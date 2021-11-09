beb554035958a6a337453f905de50907bff6c485
TheArcyie <86412656+TheArcyie@users.noreply.github.com>
2021-11-10 02:54:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var seedName = 'wheat_seeds';
 var harvestName = 'wheat';
 
 const bot = mineflayer.createBot({
-	host: "localhost",
-	username: "FarmMachine",
+	host: "anjinghitam.aternos.me",
+	username: "Petani",
 	//viewDistance: "tiny",
 });
 
